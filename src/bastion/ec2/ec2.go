64401e66e6cb02284a3630531351ccd07380061f
Cliff Moon <cliff@leaninto.it>
2015-01-15 22:43:54 -0800

    fix mebbe

--- src/bastion/ec2/ec2.go
+++ src/bastion/ec2/ec2.go
@@ -1,7 +1,7 @@
 package ec2
 
 import(
-		"fmt"
+		// "fmt"
 		"github.com/stripe/aws-go/aws"
 		"github.com/stripe/aws-go/gen/ec2"
 		"bastion/credentials"
@@ -17,10 +17,10 @@ type eC2ScannerImpl struct {
 	credProvider 	*credentials.CredentialsProvider
 }
 
-func New(credProvider *credentials.CredentialsProvider) EC2Scanner {
-	scanner := &eC2ScannerImpl{credProvider}
-
-}
+// func New(credProvider *credentials.CredentialsProvider) EC2Scanner {
+// 	scanner := &eC2ScannerImpl{credProvider}
+// 	return scanner
+// }
 
 func (s *eC2ScannerImpl) getClient() *ec2.EC2 {
 	creds := s.credProvider.GetCredentials()
@@ -28,21 +28,21 @@ func (s *eC2ScannerImpl) getClient() *ec2.EC2 {
 	return ec2.New(awsCreds, creds.Region, nil)
 }
 
-func (s *eC2ScannerImpl) ScanSecurityGroups() ([]*ec2.SecurityGroup, error) {
-	client := s.getClient()
-	resp, err := client.DescribeSecurityGroups(nil)
-	if err != nil {
-		return nil, err
-	}
-	return resp.SecurityGroups, nil
-}
+// func (s *eC2ScannerImpl) ScanSecurityGroups() ([]*ec2.SecurityGroup, error) {
+// 	client := s.getClient()
+// 	resp, err := client.DescribeSecurityGroups(nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return resp.SecurityGroups, nil
+// }
 
-func (s *EC2Scanner) ScanSecurityGroupInstances(groupId string) ([]*ec2.Reservation, error) {
-	client := s.getClient()
-	resp, err := client.DescribeInstances(&ec2.DescribeInstancesRequest{
-		Filters : []ec2.Filter{ec2.Filter{"SecurityGroups", []string{groupId}}}})
-	if err != nil {
-		return nil, err
-	}
-	return resp.Reservations, nil
-}
+// func (s *EC2Scanner) ScanSecurityGroupInstances(groupId string) ([]*ec2.Reservation, error) {
+// 	client := s.getClient()
+// 	resp, err := client.DescribeInstances(&ec2.DescribeInstancesRequest{
+// 		Filters : []ec2.Filter{ec2.Filter{"SecurityGroups", []string{groupId}}}})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return resp.Reservations, nil
+// }
