3c961231a35685d73746836418f83966714db9b4
Cliff Moon <cliff@leaninto.it>
2015-01-22 13:45:14 -0800

    point at the beta host

--- demo.sh
+++ demo.sh
@@ -2,4 +2,4 @@
 
 export GOPATH=`pwd`
 go get ./...
-go run cmd/bastion/main.go --access_key_id=a --secret_key=s --region=us-west-1 --opsee=localhost:5555 --hostname="cliff" --data=cookbooks/bastion/files/default/demo_data.json
+go run cmd/bastion/main.go --access_key_id=a --secret_key=s --region=us-west-1 --opsee=54.67.86.46:5555 --hostname="cliff" --data=cookbooks/bastion/files/default/demo_data.json
