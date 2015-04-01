610d6ba60231ccee6ab53adb4fd3185e0129f800
Andy Gross <andy@opsee.co>
2015-03-31 20:22:09 -0700

    Fresh godeps

---
+++ Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/service/elb/service.go
@@ -0,0 +1,35 @@
+package elb
+
+import (
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/internal/protocol/query"
+	"github.com/awslabs/aws-sdk-go/internal/signer/v4"
+)
+
+// ELB is a client for Elastic Load Balancing.
+type ELB struct {
+	*aws.Service
+}
+
+// New returns a new ELB client.
+func New(config *aws.Config) *ELB {
+	if config == nil {
+		config = &aws.Config{}
+	}
+
+	service := &aws.Service{
+		Config:      aws.DefaultConfig.Merge(config),
+		ServiceName: "elasticloadbalancing",
+		APIVersion:  "2012-06-01",
+	}
+	service.Initialize()
+
+	// Handlers
+	service.Handlers.Sign.PushBack(v4.Sign)
+	service.Handlers.Build.PushBack(query.Build)
+	service.Handlers.Unmarshal.PushBack(query.Unmarshal)
+	service.Handlers.UnmarshalMeta.PushBack(query.UnmarshalMeta)
+	service.Handlers.UnmarshalError.PushBack(query.UnmarshalError)
+
+	return &ELB{service}
+}
