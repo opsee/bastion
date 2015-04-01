7856182fce507acbc12e9ea86622903378f7455c
Andy Gross <andy@opsee.co>
2015-04-01 03:47:20 -0700

    Godeps

---
+++ Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/service/elb/service.go
@@ -0,0 +1,35 @@
+package elb
+
+import (
+	"github.com/awslabs/aws-sdk-go/internal/protocol/query"
+	"github.com/awslabs/aws-sdk-go/internal/signer/v4"
+	"github.com/opsee/bastion/Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws"
+)
+
+// ELB is a client for Elastic Load Balancing.
+type ELB struct {
+	*aws.Service
+}
+
+// New returns a new ELB client.
+func New(config *aws.Config) *ELB {
+	if config == nil {
+		config = &aws.Config{}
+	}
+
+	service := &aws.Service{
+		Config:      aws.DefaultConfig.Merge(config),
+		ServiceName: "elasticloadbalancing",
+		APIVersion:  "2012-06-01",
+	}
+	service.Initialize()
+
+	// Handlers
+	service.Handlers.Sign.PushBack(v4.Sign)
+	service.Handlers.Build.PushBack(query.Build)
+	service.Handlers.Unmarshal.PushBack(query.Unmarshal)
+	service.Handlers.UnmarshalMeta.PushBack(query.UnmarshalMeta)
+	service.Handlers.UnmarshalError.PushBack(query.UnmarshalError)
+
+	return &ELB{service}
+}
---
+++ Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/service/rds/service.go
@@ -0,0 +1,35 @@
+package rds
+
+import (
+	"github.com/awslabs/aws-sdk-go/internal/protocol/query"
+	"github.com/awslabs/aws-sdk-go/internal/signer/v4"
+	"github.com/opsee/bastion/Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws"
+)
+
+// RDS is a client for Amazon RDS.
+type RDS struct {
+	*aws.Service
+}
+
+// New returns a new RDS client.
+func New(config *aws.Config) *RDS {
+	if config == nil {
+		config = &aws.Config{}
+	}
+
+	service := &aws.Service{
+		Config:      aws.DefaultConfig.Merge(config),
+		ServiceName: "rds",
+		APIVersion:  "2014-10-31",
+	}
+	service.Initialize()
+
+	// Handlers
+	service.Handlers.Sign.PushBack(v4.Sign)
+	service.Handlers.Build.PushBack(query.Build)
+	service.Handlers.Unmarshal.PushBack(query.Unmarshal)
+	service.Handlers.UnmarshalMeta.PushBack(query.UnmarshalMeta)
+	service.Handlers.UnmarshalError.PushBack(query.UnmarshalError)
+
+	return &RDS{service}
+}
