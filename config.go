36666192bfc7698b0101b1c8e437a909108ef729
Andy Gross <andy@andygross.org>
2015-04-21 23:29:34 -0700

    toplevel log

--- config.go
+++ config.go
@@ -1,4 +1,16 @@
 package bastion
+import "github.com/opsee/bastion/Godeps/_workspace/src/github.com/op/go-logging"
+
+var (
+    log       = logging.MustGetLogger("bastion.json-tcp")
+    logFormat = logging.MustStringFormatter("%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}")
+)
+
+func init() {
+    logging.SetLevel(logging.DEBUG, "bastion.json-tcp")
+    logging.SetFormatter(logFormat)
+}
+
 
 type Config struct {
     accessKeyId string // AWS Access Key Id
