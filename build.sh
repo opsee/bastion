bcb4f8a6b43569b4f59927c5214c04516f08bc39
Greg Poirier <grep@binary-snobbery.com>
2015-08-11 12:00:05 -0700

    Always rebuild the protocol

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 proto_dir=proto/
 proto=${proto_dir}/checker.proto
 protoc -I/usr/local/include -I${proto_dir} --go_out=plugins=grpc:${proto_dir} ${proto}
+rm -f src/github.com/opsee/bastion/checker/checker.pb.go
+ln -s ../../../../../proto/checker.pb.go src/github.com/opsee/bastion/checker/checker.pb.go
