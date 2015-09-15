3606efc9c28e17128b8cfd26e66f2a1e6a6cd611
Greg Poirier <grep@binary-snobbery.com>
2015-09-15 16:03:30 -0700

    Move to custom import domain for google-protobuf

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ proto=${proto_dir}/checker.proto
 protoc -I/usr/local/include -I${proto_dir}/ --go_out=plugins=grpc:${proto_dir}/ ${proto}
 rm -f src/github.com/opsee/bastion/checker/checker.pb.go
 ln -s ../../../../../proto/checker.pb.go src/github.com/opsee/bastion/checker/checker.pb.go
-sed -i 's/import google_protobuf "google\/protobuf"/import google_protobuf "github.com\/peter-edge\/go-google-protobuf"/' ${proto_dir}/checker.pb.go
+sed -i 's/import google_protobuf "google\/protobuf"/import google_protobuf "go.pedge.io\/google-protobuf"/' ${proto_dir}/checker.pb.go
