5265d362683057f28d5867d0851dd8dfdc078cae
Greg Poirier <grep@binary-snobbery.com>
2015-09-20 10:21:18 -0700

    Allow build.sh to work on darwin

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+eval "$(/usr/bin/env go env)"
+
 proto_dir=proto
 proto=${proto_dir}/checker.proto
 protoc -I/usr/local/include -I${proto_dir}/ --go_out=plugins=grpc:${proto_dir}/ ${proto}
 rm -f src/github.com/opsee/bastion/checker/checker.pb.go
 ln -s ../../../../../proto/checker.pb.go src/github.com/opsee/bastion/checker/checker.pb.go
-sed -i 's/import google_protobuf "google\/protobuf"/import google_protobuf "go.pedge.io\/google-protobuf"/' ${proto_dir}/checker.pb.go
+
+if [ "$GOOS" = "darwin" ]; then
+  sed -i'' -e 's/import google_protobuf "google\/protobuf"/import google_protobuf "go.pedge.io\/google-protobuf"/' ${proto_dir}/checker.pb.go
+else
+  sed -i 's/import google_protobuf "google\/protobuf"/import google_protobuf "go.pedge.io\/google-protobuf"/' ${proto_dir}/checker.pb.go
+fi
