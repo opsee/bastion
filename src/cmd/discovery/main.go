54156933db800ad0f05e53c80f8b51fa5197a803
Dan Compton <dancompton@Dans-MacBook-Pro-2.local>
2015-09-23 16:33:09 -0700

    added vendor and updated discovery initially

--- src/cmd/discovery/main.go
+++ src/cmd/discovery/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"sync"
-	"time"
-
+	"fmt"
+	"github.com/opsee/awscan"
 	"github.com/opsee/bastion/config"
 	"github.com/opsee/bastion/heart"
 	"github.com/opsee/bastion/logging"
 	"github.com/opsee/bastion/messaging"
-	"github.com/opsee/bastion/scanner"
+	"sync"
+	"time"
 )
 
 const (
@@ -19,96 +19,24 @@ var (
 	logger   = logging.GetLogger(moduleName)
 	producer messaging.Producer
 	wg       *sync.WaitGroup
-	sc       scanner.EC2Scanner
 )
-
-func scanSecurityGroups() {
-	wg.Add(1)
-	if sgs, err := sc.ScanSecurityGroups(); err != nil {
-		logger.Error(err.Error())
-	} else {
-		for _, sg := range sgs {
-			if sg != nil {
-				if err := producer.Publish(sg); err != nil {
-					logger.Error(err.Error())
-				}
-				if sg.GroupId != nil {
-					logger.Info("Scanning SG: %v", *sg.GroupId)
-					if reservations, err := sc.ScanSecurityGroupInstances(*sg.GroupId); err != nil {
-						logger.Error(err.Error())
-					} else {
-						for _, reservation := range reservations {
-							if reservation != nil {
-								for _, instance := range reservation.Instances {
-									if instance != nil {
-										if err := producer.Publish(instance); err != nil {
-											logger.Error(err.Error())
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	wg.Done()
-}
-
-func scanLoadBalancers() {
-	wg.Add(1)
-	if lbs, err := sc.ScanLoadBalancers(); err != nil {
-		logger.Error(err.Error())
-	} else {
-		for _, lb := range lbs {
-			if lb != nil {
-				if err := producer.Publish(lb); err != nil {
-					logger.Error(err.Error())
-				}
-			}
-		}
-	}
-	wg.Done()
-}
-
-func scanRDS() {
-	wg.Add(1)
-	if rdses, err := sc.ScanRDS(); err != nil {
-		logger.Error(err.Error())
-	} else {
-		for _, rdsInst := range rdses {
-			if rdsInst != nil {
-				if err := producer.Publish(rdsInst); err != nil {
-					logger.Error(err.Error())
-				}
-			}
-		}
-	}
-	wg.Done()
-}
-
-func scanRDSSecurityGroups() {
-	wg.Add(1)
-	if rdssgs, err := sc.ScanRDSSecurityGroups(); err != nil {
-		logger.Error(err.Error())
-	} else {
-		for _, rdssg := range rdssgs {
-			if rdssg != nil {
-				if err := producer.Publish(rdssg); err != nil {
-					logger.Error(err.Error())
-				}
-			}
-		}
-	}
-	wg.Done()
-}
 
 func main() {
 	var err error
 
 	cfg := config.GetConfig()
-	sc = scanner.NewScanner(cfg)
+
+	scanner := awscan.NewScanner(&awscan.Config{AccessKeyId: cfg.AccessKeyId, SecretKey: cfg.SecretKey, Region: "us-west-1"})
+	disco := awscan.NewDiscoverer(scanner)
+
+	for event := range disco.Discover() {
+		if event.Err != nil {
+			fmt.Println("whoops: ", event.Err.Error())
+		} else {
+			fmt.Println("yay: ", event.Result)
+		}
+	}
+
 	wg = &sync.WaitGroup{}
 
 	producer, err = messaging.NewCustomerProducer(cfg.CustomerId, "discovery")
@@ -120,15 +48,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	go heart.Beat()
 
 	for {
-		go scanLoadBalancers()
-		go scanRDS()
-		go scanRDSSecurityGroups()
-		go scanSecurityGroups()
+
 		// Wait for the whole scan to finish.
 		wg.Wait()
+
 		// Sleep 2 minutes between scans.
 		time.Sleep(120 * time.Second)
 	}
